f7cfaaa2f1b3fcf6a0ec9424b330390b70d7db52
mjkim0819 <minails@naver.com>
2023-10-27 04:29:10 +0900

    :sparkles: Change lidar sensor network ros to udp

--- src/wego/scripts/lidar_node.py
+++ src/wego/scripts/lidar_node.py
@@ -1,39 +1,47 @@
 #!/usr/bin/env python3
-  
-import rospy
-from sensor_msgs.msg import PointCloud2,PointCloud
-import sensor_msgs.point_cloud2 as pc2
-from geometry_msgs.msg import Point32
 
-class lidarPaser:
+import rospy
+from sensor_msgs.msg import LaserScan, PointCloud2
+import sensor_msgs.point_cloud2 as pc2
+import math
+
+class LidarParser:
     def __init__(self):
-        rospy.init_node('lidar', anonymous=True)
-    
+        rospy.init_node('lidar_parser', anonymous=True)
         rospy.Subscriber("/velodyne_points", PointCloud2, self.callback)
-        self.pc1_pub = rospy.Publisher('/scan',PointCloud, queue_size=1)
-        
-        
+        self.scan_pub = rospy.Publisher('/scan', LaserScan, queue_size=1)
         rospy.spin()
 
     def callback(self, data):
-        pc1_msg=PointCloud()
-        for p in pc2.read_points(data, field_names = ("x", "y", "z"), skip_nans=True):
-            tmp_point=Point32()
-            tmp_point.x=p[0]
-            tmp_point.y=p[1]
-            tmp_point.z=p[2]
-            pc1_msg.points.append(tmp_point)
+        scan_msg = LaserScan()
+        scan_msg.header = data.header
+        scan_msg.angle_min = -math.pi
+        scan_msg.angle_max = math.pi
+        scan_msg.angle_increment = 0.0174532925199  # Set the angle increment (1 degree in radians)
+        scan_msg.time_increment = 0.0
+        scan_msg.scan_time = 0.0
+        scan_msg.range_min = 0.0
+        scan_msg.range_max = 100.0
 
+        ranges = []  # Create an empty list to store the range data
 
-        pc1_msg.header.frame_id=data.header.frame_id
-        pc1_msg.header.stamp=data.header.stamp
-        self.pc1_pub.publish(pc1_msg)
-        # print("point num : {}".format(len(pc1_msg.points)))
-        
-        
+        for angle in range(0, 360):  # Assuming 360 degrees LiDAR data
+            # Calculate the range from the (x, y, z) point
+            range_val = self.get_range_at_angle(data, math.radians(angle))
+            ranges.append(range_val)
+
+        scan_msg.ranges = ranges
+        self.scan_pub.publish(scan_msg)
+
+    def get_range_at_angle(self, point_cloud, angle):
+        for p in pc2.read_points(point_cloud, field_names=("x", "y", "z"), skip_nans=True):
+            point_angle = math.atan2(p[1], p[0])  # Calculate the angle of the point
+            if math.isclose(point_angle, angle, abs_tol=0.0174532925199):
+                return math.sqrt(p[0] ** 2 + p[1] ** 2 + p[2] ** 2)
+        return float('inf')
 
 if __name__ == '__main__':
     try:
-        lidar = lidarPaser()
+        lidar_parser = LidarParser()
     except rospy.ROSInterruptException:
         pass
